3bdbf9a9a8edf2948e8fd1b62baa95e0bb5547e5
Serial-Gemini <waylonchester94@gmail.com>
2025-05-27 22:17:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function startImageLoop() {
                 currentImageIndex = 0;
             }
             imageEl.src = images[currentImageIndex];
-        }, 43200000); // 12 hours
+        }, 5000); // 12 hours
     }
 }
 
